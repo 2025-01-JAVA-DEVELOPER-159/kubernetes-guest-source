9be83ad440395fb58fbffbcdb8666ed67e2268a6
academyitwill <academyitwill@gmail.com>
2024-12-26 19:01:17 +0900

    ..

--- src/main/java/com/itwill/GuestBackendApplication.java
+++ src/main/java/com/itwill/GuestBackendApplication.java
@@ -22,7 +22,7 @@ public class GuestBackendApplication  implements CommandLineRunner{
 	private GuestService guestService;
 	@Override
 	public void run(String... args) throws Exception {
-		for(int i=0;i<3;i++){
+		for(int i=0;i<20;i++){
 		guestService.guestWrite(GuestDto.builder()
 						.guestName("guest"+i)
 						.guestEmail("guest"+i+"@gmail.com")
